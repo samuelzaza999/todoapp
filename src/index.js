5e1640ccf85bdb0d358366de0e1b809bab8ee7e7
samuelzaza999 <31839497+samuelzaza999@users.noreply.github.com>
2022-05-26 16:33:23 +0900

    form 이벤트 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import { log } from "./utils";
 import "./todos.css";
 
-log("webpack5 boilerplate");
+const todos = [];
+
+// 폼에 입력 이벤트 추가하기 & 입력 시 todo 추가
+const $form = document.querySelector(".new-task");
+const $input = document.querySelector(".new-task > input");
+
+const checkTodo = (e) => {
+  e.preventDefault();
+
+  // todo 추가
+  const todo = { title: $input.value, isDone: false };
+  todos.push(todo);
+  console.log(todos);
+
+  // 폼 초기화
+
+  // todos 출력
+};
+$form.addEventListener("submit", checkTodo);
+
+// todos 뿌리기
