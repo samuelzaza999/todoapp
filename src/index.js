a2e067370dab6991f92c7593c0af546e9056f33c
samuelzaza999 <31839497+samuelzaza999@users.noreply.github.com>
2022-05-26 16:41:48 +0900

    todos 출력

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import { log } from "./utils";
 import "./todos.css";
 
-const todos = [];
+const todos = [
+  { title: "샌즈", isDone: false },
+  { title: "파피루스", isDone: false },
+]; // 기본 입력형
 
 // 폼에 입력 이벤트 추가하기 & 입력 시 todo 추가
 const $form = document.querySelector(".new-task");
 const $input = document.querySelector(".new-task > input");
+const $todos = document.querySelector("#todos");
+
+const printTodos = () => {
+  const html = todos.map((todo) => {
+    return `
+    <ul>
+        <li>
+          <button class="delete">×</button>
+          <input type="checkbox" class="toggle-checked" />
+          <span class="text">${todo.title}</span>
+        </li>
+      </ul>
+    `;
+  });
+  $todos.innerHTML = `<ul>${html.join("")}</ul>`;
+};
 
 const checkTodo = (e) => {
   e.preventDefault();
@@ -16,9 +35,12 @@ const checkTodo = (e) => {
   console.log(todos);
 
   // 폼 초기화
+  $input.value = "";
 
   // todos 출력
+  printTodos();
 };
 $form.addEventListener("submit", checkTodo);
 
-// todos 뿌리기
+// 시작 시 todos 출력
+printTodos();
